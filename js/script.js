6f0c110eb373a92ee2c61812474514248f78c743
unknown <amierzwinska@gmail.com>
2018-03-03 20:04:58 +0100

    Final version

--- js/script.js
+++ js/script.js
@@ -1,43 +1,40 @@
-
 // select all project divs into array
 const projectItems = Array.from(document.getElementsByClassName('a-projectBar__item'));
 
 // enable and listen for toggling featured project class on project divs
 const toggleFeature = (e) => {
 
-  let currFeature = projectItems.find(el => el.classList.contains('a-projectBar__feature'));
-  console.log('Feat: ', currFeature, ' Targ:', e.target);
-  if((e.target == currFeature) || (e.target.parentElement == currFeature) || (e.target.classList.contains('a-projectBar__imgCaption'))){ /// add condition for clicking text part of projectItem
-    return;
-  }
-  // point to parent (item) if child (img) clicked
-  let newFeature = e.target.classList.contains('a-projectBar__item') ? e.target : e.target.parentElement;
+	let currFeature = projectItems.find(el => el.classList.contains('a-projectBar__feature'));
+	if ((e.target == currFeature) || (e.target.parentElement == currFeature) || (!e.target.classList.contains('a-projectBar__img'))) {
+		return;
+	}
 
-  projectItems.forEach(el => el.classList.remove('fade-in'))
-  projectItems.forEach(el => el.classList.add('fade-out'))
+	let newFeature = e.target.parentElement;
+	projectItems.forEach(el => el.classList.remove('fade-in'))
+	projectItems.forEach(el => el.classList.add('fade-out'))
 
-  setTimeout(() => {
-    currFeature.classList.remove('a-projectBar__feature');
-    newFeature.classList.add('a-projectBar__feature');
-    projectItems.forEach(el => el.classList.add('fade-in'))
-  }, 300)
+	setTimeout(() => {
+		currFeature.classList.remove('a-projectBar__feature');
+		newFeature.classList.add('a-projectBar__feature');
+		projectItems.forEach(el => el.classList.add('fade-in'))
+	}, 300)
 }
 
 // toggle feature swap, feature styling on various screen sizes
 const enableFeatureStyling = () => {
-  if(window.innerWidth > 920){
-      projectItems.forEach(item => item.addEventListener('click', toggleFeature));
-      if(!(projectItems.find(el => el.classList.contains('a-projectBar__feature')))){
-        projectItems[0].classList.add('a-projectBar__feature');
-      }
-  } else {
-      projectItems.forEach(item => item.removeEventListener('click', toggleFeature));
-      projectItems.forEach(item => {
-        if(item.classList.contains('a-projectBar__feature')){
-          item.classList.remove('a-projectBar__feature');
-        }
-      });
-  }
+	if (window.innerWidth > 920) {
+		projectItems.forEach(item => item.addEventListener('click', toggleFeature));
+		if (!(projectItems.find(el => el.classList.contains('a-projectBar__feature')))) {
+			projectItems[0].classList.add('a-projectBar__feature');
+		}
+	} else {
+		projectItems.forEach(item => item.removeEventListener('click', toggleFeature));
+		projectItems.forEach(item => {
+			if (item.classList.contains('a-projectBar__feature')) {
+				item.classList.remove('a-projectBar__feature');
+			}
+		});
+	}
 }
 window.onload = enableFeatureStyling;
 window.addEventListener('resize', enableFeatureStyling);
@@ -45,9 +42,12 @@ window.addEventListener('resize', enableFeatureStyling);
 
 /// toggle menuIcon
 const hamburger = document.getElementById('menuIcon');
+const navLinks = Array.from(document.getElementsByClassName('a-sidebar__item'));
 const sidebar = document.getElementById('sidebar');
 
 const toggleMenu = () => {
-  sidebar.classList.toggle('-open');
+	sidebar.classList.toggle('-open');
 }
+
 hamburger.addEventListener('click', toggleMenu);
+navLinks.forEach(link => link.addEventListener('click', toggleMenu));
